52eb9fec9edbab7ba7639de8a4d8f066021fbba7
JsCyberWar <bile2804@gmail.com>
2020-11-06 14:17:19 +0700

    predent

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -9,7 +9,7 @@ import { FormBuilder} from '@angular/forms';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
+  isCorrectForm='white';
   checkoutForm;
 
   constructor(private http: HttpClient, private formBuilder: FormBuilder) {
@@ -26,11 +26,19 @@ export class ContactComponent implements OnInit {
  faMapMarkerAlt = faMapMarkerAlt;
  faPhone = faPhone;
 
- readonly ROOT_URL: string = 'http://localhost:3000'; 
-
+ readonly ROOT_URL: string = 'http://localhost:3000/subcriber'; 
+validateEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
  sendData(data){
-   this.http.post(this.ROOT_URL,data).subscribe();
-   console.log(data);
+   if(this.validateEmail(data))
+    this.http.post(this.ROOT_URL,data).subscribe(() => {
+      window.location.reload();
+    });
+    else {
+      this.isCorrectForm='red'
+    }
  }
 
 }
